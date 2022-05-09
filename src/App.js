c894ead97acc2141d0a115832e6af45f3419e64a
krishbhk <k.rishabh2899@gmail.com>
2022-05-09 17:03:31 +0530

    ABout need to be implemented

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+import About from "./Components/About";
 import Navbar from "./Components/Navbar";
-import TextForm from "./Components/TextForm";
+// import TextForm from "./Components/TextForm";
 
 function App() {
   return (
-  // <Navbar title='TextUtils' about="About TextUtils"/>
-  <>
+  <div>
     <Navbar title='TextUtils' />
-    <div className="container my-5">
-    <TextForm heading="Enter the Text to analyze"/>
+    <div className="container my-4">
+    {/* <TextForm heading="Enter the Text to analyze"/> */}
+    <About />
     </div>
-  </>
+  </div>
   );
 }
 
--- src/Components/TextForm.jsx
+++ src/Components/TextForm.jsx
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import PropTypes from 'prop-types'
 
-import ButtonCase from "./ButtonCase";
 import TextSummary from "./TextSummary";
-
 
 export default function TextForm(props) {
 
-    let [text, setText] = useState();
-    let [newText, setNewText] = useState();
+    let [text, setText] = useState('');
+    let [newText, setNewText] = useState('');
 
     let handleUCClick = ()=>{
         // console.log('HandleUCClick was clicked');
@@ -16,13 +14,21 @@ export default function TextForm(props) {
     }
 
     let handleLCClick = ()=>{
-        // console.log('HandleUCClick was clicked');
         setNewText(text.toLowerCase());
+    }
+
+    let handleClearClick = ()=>{
+        setText('');
+        setNewText('');
     }
 
     let handleOnChange = (event)=>{
         // console.log('Onchanged');
         setText(event.target.value);
+    }
+
+    let handleCopy = ()=>{
+        navigator.clipboard.writeText(newText);
     }
 
   return (
@@ -32,8 +38,9 @@ export default function TextForm(props) {
             <textarea value={text} onChange={handleOnChange} placeholder="Enter your text here" className="form-control" id="myBox" rows="8" />
         </div>
 
-        <ButtonCase className="my-5" onClick={handleUCClick} case = "Upper Case"/>
-        <ButtonCase className="ms-2" onClick={handleLCClick} case = "Lower Case"/>        
+        <button className="btn btn-outline-dark me-2" onClick={handleUCClick}>Convert to Upper Case</button>
+        <button className="btn btn-outline-dark me-2" onClick={handleLCClick}>Convert to Lower Case</button>
+        <button className="btn btn-outline-danger float-end" onClick={handleClearClick}> Clear Text </button>
 
         <TextSummary wordCount={text ? text.split(" ").length : 0} characterCount = {text ? text.length : 0} time={text ? 0.004 * text.split(" ").length : 0} />
 
@@ -41,6 +48,7 @@ export default function TextForm(props) {
             <h3><label htmlFor="myBox"> Convereted Text </label></h3>
             <div className="card">
                 <div className="card-body">
+                    <button className="btn btn-outline-secondary btn-sm float-end" onClick={handleCopy}>Copy</button>
                     <div className="card-text">{newText}</div>
                 </div>
             </div>
